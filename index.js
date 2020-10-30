610882003a23f0dc33daea30767040d00ca5ed5e
Brian Kubes <briankubes@gmail.com>
2020-10-29 18:30:18 -0700

    removed server to server.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 require('dotenv').config()
+const server = require('./server.js')
 
-const express = require('express')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const cors = require('cors')
-require('colors')
-
-const server = express()
-
-server.use(helmet())
-server.use(morgan('dev'))
-server.use(cors())
-server.use(express.json())
-
-server.get('/', (req, res)=>{
-    res.send('endpoint running')
-})
 const port = process.env.PORT || 5000
 
 server.listen(port, ()=> {
